ee4944c4ea3abeb4984e026cf91881fc715a356f
Shadan <shadan.zahid@microsoft.com>
2023-03-09 17:34:31 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module shadan/go-first
+
+go 1.2
+
+replace autogen-gocode => ../GoModules/discoveryclusterextensioncontract
+
+require autogen-gocode v0.0.0-00010101000000-000000000000
---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt";
+	"autogen-gocode";
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	msg := discoveryclusterextensioncontract.GetUnhandledException("arg1", "arg2", "agr3")
+	fmt.Println(msg)
+}
